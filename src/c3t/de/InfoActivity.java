4258521e0478e79afcb02e34a3c30633c5803a11
fidepus <mail@fidepus.de>
2010-10-21 16:11:22 +0200

    added map (doesn't work)

--- src/c3t/de/InfoActivity.java
+++ src/c3t/de/InfoActivity.java
@@ -1,17 +1,28 @@
 package c3t.de;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
-public class InfoActivity extends Activity {
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
 
+public class InfoActivity extends MapActivity {
+	@Override
+	protected boolean isRouteDisplayed() {
+	    return false;
+	}
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.info);
+	    MapView mapView = (MapView) findViewById(R.id.mapview);
+	    mapView.setBuiltInZoomControls(true);
+	}
 
-    public void onCreate(Bundle savedInstanceState) {
+ /*   public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         TextView textview = new TextView(this);
         textview.setText("Hier ist später die Karte");
         setContentView(textview);
-    }
+    } */
 }

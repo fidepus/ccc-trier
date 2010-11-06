805470c7e84547bc4af4dc683641e5c266132e38
Oliver Leitzgen <oliver@leitzgen.com>
2010-11-06 20:31:14 +0100

    typo fixed

--- src/c3t/de/StatusActivity.java
+++ src/c3t/de/StatusActivity.java
@@ -9,16 +9,22 @@ public class StatusActivity extends Activity {
 	
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.status);     
+        setContentView(R.layout.status);
+    }
+    
+    public void onStart() 
+    {
+        super.onStart();
+        setStatusOn();
     }
     
     void setStatusOn() {
-    	 findViewById( R.id.LinearLayout01).setBackgroundResource(R.drawable.led_an);
+    	 findViewById(R.id.LinearLayout01).setBackgroundResource(R.drawable.led_an);
     	 isOn = true;
     }
     
     void setStatusOff() {
-    	findViewById( R.id.LinearLayout01).setBackgroundResource(R.drawable.led_an);
+    	findViewById(R.id.LinearLayout01).setBackgroundResource(R.drawable.led_aus);
     	isOn = false;
     }
     

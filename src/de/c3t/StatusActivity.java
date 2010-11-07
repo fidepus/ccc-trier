36d7d00de642bad9fad0853cc3a0d1d676d7cc13
Oliver Leitzgen <oliver@leitzgen.com>
2010-11-07 01:34:36 +0100

    changed: instead fullscreen unscalabe logo using gradient draw with centered logo

--- src/de/c3t/StatusActivity.java
+++ src/de/c3t/StatusActivity.java
@@ -19,6 +19,7 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class StatusActivity extends Activity {
 
@@ -27,6 +28,7 @@ public class StatusActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.status);
+		findViewById(R.id.StatusLayout).getBackground().setDither(true);
 	}
 
 	public void onResume() {
@@ -38,12 +40,16 @@ public class StatusActivity extends Activity {
 	}
 
 	void setStatusOn() {
-		findViewById(R.id.LinearLayout01).setBackgroundResource(R.drawable.status_led_on);
+		findViewById(R.id.StatusLayout).setBackgroundResource(R.drawable.status_on);
+		ImageView image = (ImageView) findViewById(R.id.StatusLogo);
+		image.setImageResource(R.drawable.status_led_on);
 		isOn = true;
 	}
 
 	void setStatusOff() {
-		findViewById(R.id.LinearLayout01).setBackgroundResource(R.drawable.status_led_off);
+		findViewById(R.id.StatusLayout).setBackgroundResource(R.drawable.status_off);
+		ImageView image = (ImageView) findViewById(R.id.StatusLogo);
+		image.setImageResource(R.drawable.status_led_off);
 		isOn = false;
 	}
 

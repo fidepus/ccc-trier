535c1aeed8cd00a3307f8ebefc7d57991893ee6a
Kevin Kirsch <keine.ahnung.ka@googlemail.com>
2011-07-18 15:12:14 +0200

    Reverted some of the bad things i did in "changeset 52:d1e089a3c271"

--- src/de/c3t/CCCTrier.java
+++ src/de/c3t/CCCTrier.java
@@ -12,14 +12,12 @@ public class CCCTrier extends TabActivity
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		C2DM.registerC2DM(this); //i'll change this back when i finished debugging c2dm with an emulator without all libs
-		/*
+		C2DM.registerC2DM(this);
 
 		startService(new Intent(this, OnlineService.class));
 		TabHost tabHost = getTabHost();
 		tabHost.addTab(tabHost.newTabSpec("1").setIndicator("News", getResources().getDrawable(R.drawable.ic_tab_news)).setContent(new Intent(this, RssList.class)));
 		tabHost.addTab(tabHost.newTabSpec("2").setIndicator("Status", getResources().getDrawable(R.drawable.ic_tab_status)).setContent(new Intent(this, StatusActivity.class)));
 		tabHost.addTab(tabHost.newTabSpec("3").setIndicator("Map", getResources().getDrawable(R.drawable.ic_tab_info)).setContent(new Intent(this, NaviActivity.class)));
-*/
 	}
 }

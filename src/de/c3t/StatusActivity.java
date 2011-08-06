041d4c423cf3c23040be648ef263e6759b81b52c
fidepus <mail@fidepus.de>
2011-08-07 00:39:33 +0200

    Cleaned it up and changed some dates to make it fit for publishing.

--- src/de/c3t/StatusActivity.java
+++ src/de/c3t/StatusActivity.java
@@ -1,12 +1,6 @@
 package de.c3t;
 
 import android.app.Activity;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ImageView;
 

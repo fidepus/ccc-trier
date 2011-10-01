dac0100718e99c0fc6f539646abcf2f99c96783d
fidepus <mail@fidepus.de>
2011-10-01 23:16:10 +0200

    Get current location from network or GPS and display it on the map
    Next: draw the route between current location and geopoint of club.

--- src/de/c3t/NaviActivity.java
+++ src/de/c3t/NaviActivity.java
@@ -4,15 +4,19 @@ import java.util.List;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
+import com.google.android.maps.MyLocationOverlay;
 import com.google.android.maps.Overlay;
 import com.google.android.maps.OverlayItem;
 
 public class NaviActivity extends MapActivity {
+	private MapView mapView;
+	private MyLocationOverlay myLocationOverlay;
 	@Override
 	protected boolean isRouteDisplayed() {
 		return false;
@@ -21,22 +25,55 @@ public class NaviActivity extends MapActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		// navi.xml contains a MapView
 		setContentView(R.layout.navi);
+		// extract MapView from layout
 		MapView mapView = (MapView) findViewById(R.id.mapview);
 		MapController mapController = mapView.getController();
 		mapController.setZoom(15);
 		GeoPoint point = new GeoPoint(49764708, 6652758);
 		mapController.animateTo(point);
 		mapView.setBuiltInZoomControls(true);
+		
+		// create an overlay that shows our current location
+		myLocationOverlay = new MyLocationOverlay(this, mapView);
 
+		// add this overlay to the MapView and refresh it
 		List<Overlay> mapOverlays = mapView.getOverlays();
 		Drawable drawable = this.getResources().getDrawable(R.drawable.pin);
 		NaviItemizedOverlay itemizedoverlay = new NaviItemizedOverlay(drawable, this);
+		mapView.getOverlays().add(myLocationOverlay);
+		mapView.postInvalidate();
 
 		OverlayItem overlayitem = new OverlayItem(point, "CCC Trier", "Paulinstr. 123");
 
 		itemizedoverlay.addOverlay(overlayitem);
 		mapOverlays.add(itemizedoverlay);
+	}
+	
+    @Override
+	protected void onResume() {
+		super.onResume();
+		// when our activity resumes, we want to register for location updates
+		myLocationOverlay.enableMyLocation();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		// when our activity pauses, we want to remove listening for location updates
+		myLocationOverlay.disableMyLocation();
+	}
+	
+	private void zoomToMyLocation() {
+		GeoPoint myLocationGeoPoint = myLocationOverlay.getMyLocation();
+		if(myLocationGeoPoint != null) {
+			mapView.getController().animateTo(myLocationGeoPoint);
+			mapView.getController().setZoom(10);
+		}
+		else {
+			Toast.makeText(this, "Cannot determine location", Toast.LENGTH_SHORT).show();
+		}
 	}
 
 	/*
